585d5756b50862b7b86124eae30a15122b4b0289
Zain <danbabagroup96@gmail.com>
2024-10-19 17:41:57 +0100

    f

--- js/main.js
+++ js/main.js
@@ -10,25 +10,25 @@
 /*=========================================================================
     	Sticky Header
 =========================================================================*/ 
-    $(function() {
-        var header = $("#header"),
-            height = header.height(),
-            yOffset = 0,
-            triggerPoint = 100;
-        $('.header-height').css('height', height+'px');
-        $(window).on( 'scroll', function() {
-            yOffset = $(window).scrollTop();
+    // $(function() {
+    //     var header = $("#header"),
+    //         height = header.height(),
+    //         yOffset = 0,
+    //         triggerPoint = 100;
+    //     $('.header-height').css('height', height+'px');
+    //     $(window).on( 'scroll', function() {
+    //         yOffset = $(window).scrollTop();
 
-            if (yOffset >= triggerPoint) {
-            	header.removeClass("animated cssanimation fadeIn");
-                header.addClass("navbar-fixed-top  cssanimation animated fadeInTop");
-            } else {
-                header.removeClass("navbar-fixed-top cssanimation  animated fadeInTop");
-                header.addClass("animated cssanimation fadeIn");
-            }
+    //         if (yOffset >= triggerPoint) {
+    //         	header.removeClass("animated cssanimation fadeIn");
+    //             header.addClass("navbar-fixed-top  cssanimation animated fadeInTop");
+    //         } else {
+    //             header.removeClass("navbar-fixed-top cssanimation  animated fadeInTop");
+    //             header.addClass("animated cssanimation fadeIn");
+    //         }
 
-        });
-    });
+    //     });
+    // });
 
 /*=========================================================================
     Nivo slider 
